7a166a49bc6855283063e4b9143a9e31f7416937
Sampat55 <k.shiva091272@gmail.com>
2023-09-20 16:24:00 +0530

    initial commit V11

---
+++ cal.sh
@@ -0,0 +1,9 @@
+echo "This is implemented by Anwesha Das"
+
+read -p "Enter the first side: " a
+read -p "Enter the second side: " b
+read -p "Enter the third side: " c
+s=`echo "scale=4; ($a+$b+$c)/2" | bc`
+area=`echo "scale=4; sqrt($s*($s-$a)*($s-$b)*($s-$c))" | bc`
+echo "The area of the triangle is $area"
+echo "This is V1.o file"
